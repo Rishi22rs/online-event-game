02bd58f98cb94d36a03bb05f2d91fe34171bdc88
Rishi Srivastava <rishisrivastava22rs@gmail.com>
2020-05-08 00:21:48 +0530

    Added instruction modal

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,9 @@
 import React,{useState} from 'react'
 import {Redirect} from 'react-router-dom'
 import axios from 'axios'
+import Modal from 'react-modal'
 
+Modal.setAppElement('#root')
 const Home=()=>{
     const [detail,setDetail]=useState({
         name:'',
@@ -10,6 +12,7 @@ const Home=()=>{
         college:'' 
     })
     const [error,setError]=useState(0)
+    const [showModal,setShowModal]=useState(true)
 
     const[go,setGo]=useState(false)
 
@@ -47,6 +50,22 @@ const Home=()=>{
     }
     return(
         <div className="App">
+            <Modal 
+                isOpen={showModal}
+                style={{
+                    content:{
+                        backgroundColor:'#252C4A',
+                        color:'#949EC4'
+                    }
+                }}
+            >
+				<h2 style={{color:'white',textDecoration: 'underline'}}>Instructions</h2>
+				<p><span style={{color:'white'}}>1)</span>	A time limit of 20 seconds will be given in which the participants will have to listen to an audio and enter their responses in the text box.</p>
+                <p><span style={{color:'white'}}>2)</span>	The participants will have to manually play the audio by clicking on the option present at the left side of the screen.</p>
+                <p><span style={{color:'white'}}>3)</span>	The participants have to submit their responses according to the character limit that is mentioned at the top right corner of the text box.</p>
+                <p><span style={{color:'white'}}>4)</span>	The inputs entered aren’t case sensitive.</p>
+                <button className='btn' onClick={()=>setShowModal(false)} style={{fontSize:20,float:'right',padding:25}}>Okay</button>
+			</Modal>
             <h1>Guess the brand</h1>
             <button className="btn" onClick={CheckUser}>Start</button><br />
             {error===1?<p style={{textAlign:'center',color:'red'}}>*Fill all the fields</p>:<></>}
