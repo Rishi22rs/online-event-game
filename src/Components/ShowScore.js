01298ca989b8332236531667c42a0de3eeccbc4c
Rishi Srivastava <rishisrivastava22rs@gmail.com>
2020-05-22 21:49:01 +0530

    added my game link

--- src/Components/ShowScore.js
+++ src/Components/ShowScore.js
@@ -21,6 +21,8 @@ const ShowScore=()=>{
 			<h1 className="text" style={{borderBottom:'2px solid white',paddingBottom:20}}>Final score</h1>
 			<h2 className="text" style={{color:'white'}}>{score}/10</h2>
 			<p className="text" style={{fontSize:30}}>Well played</p>
+			<p className="text">Check out my game first indie game for android devices.</p>
+			<p className="text"><a style={{color:'white'}} href="https://play.google.com/store/apps/details?id=com.UnderratedGames.HowLong">Just click here</a></p>
 		</div>
 		<div class='ripple-background' style={{zIndex:-100,position:'absolute'}}>
             <div class='circle xxlarge shade1'></div>
